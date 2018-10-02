8f2d0b808fe7307e6d1fe9f70ed36a0a309d3c9e
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2018-10-02 16:44:31 +0800

    Update RSEM_quant.sh

--- RSEM_quant.sh
+++ RSEM_quant.sh
@@ -6,6 +6,6 @@ set -x
  echo "$1 file"
   python run_RSEM.py \
         $base_dir/rsem_reference \
-        $data_dir/$1.Deduped.Aligned.sortedByCoord.out.bam \
+        $data_dir/$1 \
         $data_dir/$1 \
 --threads 4
