646d2374836b2c3a290990b022c48164fd15c327
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2018-10-02 15:12:34 +0800

    Update RNASeQC.sh

--- RNASeQC.sh
+++ RNASeQC.sh
@@ -5,7 +5,7 @@ data_dir=$base_dir/data
 set -x
  echo "$1 file"
   python3 run_rnaseqc.py \
-  $1.Aligned.sortedByCoord.out.patched.md.bam \
+  $1.Deduped.Aligned.sortedByCoord.out.bam \
   $base_dir/gencode.v19.annotation.patched_contigs.gtf \
   $base_dir/rsem_reference/rsem_reference.transcripts.fa \
   $1 \
