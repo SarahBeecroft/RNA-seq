cf78ef5523ccf3f37acb726560b6a4a96cad16b8
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2018-10-03 12:03:15 +0800

    Update mark_dup.sh

--- mark_dup.sh
+++ mark_dup.sh
@@ -3,8 +3,8 @@ set -x
 echo "$1 file"
  java '-Djava.io.tmpdir=/data/tmp' -jar ~/bin/picard.jar MarkDuplicates \
         INPUT=$1 \
-        OUTPUT=$1.Deduped.sortedByCoord.Aligned.out.bam \
-        METRICS_FILE=$1.sorted.deduped.metrics \
+        OUTPUT=Deduped.$1 \
+        METRICS_FILE=$1.MarDup.metrics \
         CREATE_INDEX=TRUE \
         SORTING_COLLECTION_SIZE_RATIO=0.1 \
         ASSUME_SORTED=TRUE \
